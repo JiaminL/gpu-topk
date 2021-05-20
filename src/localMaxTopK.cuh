ea3c1959103baf6c76ee86e013ae197771963c1b
Jiamin Lu <2793543190@qq.com>
2021-05-20 18:34:38 +0800

    update local Max Topk

--- src/localMaxTopK.cuh
+++ src/localMaxTopK.cuh
@@ -20,30 +20,34 @@ __global__ void getLocalMax(KeyT *d_keys) {
     d_keys[offset] = max;
 }
 
-template <typename KeyT, int KeysPerThread, int LoopTimes>
+template <typename KeyT, int KeysPT, int MaxItems>
 __global__ void getLocalMax2(KeyT *d_keys) {
-    KeyT x[KeysPerThread];
-    int tCur = threadIdx.x * KeysPerThread;
-    int keys_per_loop = KeysPerThread * blockIdx.x * blockDim.x;
-    d_keys += keys_per_loop;
-    for (int j = 0; j < KeysPerThread; j++) x[j] = d_keys[tCur + j];
-    for (int i = 1; i < LoopTimes; i++) {
+    KeyT x[KeysPT];
+    int offset = KeysPT * (blockIdx.x * blockDim.x + threadIdx.x);
+    int keys_per_loop = KeysPT * blockDim.x * gridDim.x;
+
+    d_keys += offset;
+    for (int j = 0; j < KeysPT; j++) x[j] = d_keys[j];
+    for (int i = 1; i < MaxItems; i++) {
         d_keys += keys_per_loop;
-        for (int j = 0; j < KeysPerThread; j++) x[j] = max(d_keys[tCur + j], x[j]);
+        for (int j = 0; j < KeysPT; j++) x[j] = max(d_keys[j], x[j]);
     }
-    for (int j = 0; j < KeysPerThread; j++) d_keys[tCur + j] = x[j];
+    d_keys -= (MaxItems - 1) * keys_per_loop;
+    for (int j = 0; j < KeysPT; j++) d_keys[j] = x[j];
 }
 
+
+#define LOCAL_MAX_ITEMS 16
+#define LOCAL_MAX_KPT 4
 template <typename KeyT>
 cudaError_t localMaxTopK(KeyT *d_keys_in, unsigned int num_items, unsigned int k, KeyT *d_keys_out, unsigned int &out_items,
                          CachingDeviceAllocator &g_allocator) {
     out_items = k;
 
     if (num_items >= 64 * k) {
-        uint block_size = 512;
-        // getLocalMax<KeyT, 1, 8><<<num_items / block_size / 8 / 1, block_size>>>(d_keys_in);
-        getLocalMax<KeyT, 8><<<num_items / block_size / 8 / 1, block_size>>>(d_keys_in);
-        num_items /= 8;
+        uint block_size = 1024;
+        getLocalMax2<KeyT, LOCAL_MAX_KPT, LOCAL_MAX_ITEMS><<<num_items / block_size / LOCAL_MAX_KPT / LOCAL_MAX_ITEMS, block_size>>>(d_keys_in);
+        num_items /= LOCAL_MAX_ITEMS;
     }
 
     uint log2_n = log2_32(num_items);
